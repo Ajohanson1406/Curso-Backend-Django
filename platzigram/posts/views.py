605a1f3d4a9f6c1025d988dbbfc499d7441b6b38
Albert <alberttruk1406@hotmail.es>
2020-10-06 18:13:56 -0600

    Using template system Django

--- platzigram/posts/views.py
+++ platzigram/posts/views.py
@@ -1,3 +1,46 @@
+"""posts views."""
+
+#Django
 from django.shortcuts import render
+#Utilities
+from datetime import datetime
+
+
+posts = [
+    {
+        'title': 'Blackie',
+        'user': {
+            'name': 'Barbara Johanson',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timesstamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/id/237/200/200',
+    },
+    {
+        'title': 'Shu Swamp',
+         'user': {
+            'name': 'Christian van der henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timesstamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/id/84/200/200',
+    },
+    {
+        'title': 'Rainy Day',
+         'user': {
+            'name': 'Albert Johanson',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timesstamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/id/784/200/200',
+    }
+]
+
 
 # Create your views here.
+
+def list_posts(request):
+    """list existing posts."""
+
+    return render(request, 'feed.html', {'posts': posts})
+   
