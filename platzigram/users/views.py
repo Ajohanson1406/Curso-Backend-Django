612376bcdb69d3e4d7bfe36bbc6f3f4a57bfaa05
Albert Johanson <alberttruk.11@gmail.com>
2020-10-29 10:25:50 -0600

    Adding the template to update user

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -12,6 +12,42 @@ from django.db.utils import IntegrityError
 from django.contrib.auth.models import User
 from users.models import Profile
 
+#Forms
+from users.forms import ProfileForm
+
+@login_required
+def update_profile(request):
+    """update a users's profile view"""
+
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+
+    else:
+        form = ProfileForm()
+
+    return render(
+        request=request,
+        template_name='users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            'form': form
+        }
+    )
+
+
 
 def login_view(request):
     username = request.POST.get('username', True)
